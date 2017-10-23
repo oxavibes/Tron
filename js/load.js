5fb6b357c6403a7ba7547849630379bb27e4906d
Oxavibes <katteos@hotmail.com>
2017-10-23 06:46:40 -0400

    Menu design updated

--- js/load.js
+++ js/load.js
@@ -9,10 +9,10 @@ var loadState = {
         game.scale.PageAlignHorizontally = true;
         game.scale.pageAlignVertically = true;
         
-        game.load.image('background', 'assets/sprites/background_3.png');
-        //game.load.image('new_game_button', 'assets/sprites/grey_button03.png');
-        //game.load.image('quit_game_button_down', 'assets/sprites/blue_button01.png');
-        game.load.spritesheet('dude', 'assets/sprites/dude.png', 32, 48);
+        game.load.image('initialbackground', 'assets/background/initialbackground.png');
+        game.load.spritesheet('new_game_button', 'assets/buttons/new_game.png', 100, 17);
+        game.load.spritesheet('edit_map_button', 'assets/buttons/map_editor_button.png', 125, 18);
+        game.load.spritesheet('quit_button', 'assets/buttons/quit_button.png', 50, 18);
 
 
         game.load.audio('main_music','assets/music/main-music.ogg');
--- js/play.js
+++ js/play.js
@@ -1,11 +1,10 @@
 var playState = {
     player: null,
     create: function(){
-        
-        player = game.add.sprite(300, 200, 'dude');
+      
         
     },
     update: function(){
-
+           
     }
 };
